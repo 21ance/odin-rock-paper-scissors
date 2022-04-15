0fae5003159edd5c84812f8e2d00b36e813a0466
LanceL <lancelopez46@gmail.com>
2022-04-15 19:02:51 +0800

    Added a UI that works with the JavaScript

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-Hello, this is a basic rock paper scissors game using JavaScript.
+This is a rock paper scissors game using JavaScript - the user is playing against the computer.
 
-The game has no GUI. Although it prompts the user for input, the results can only be seen in the console.
+HTML and CSS
+    - The UI is split into three parts: (1)Header, (2)Main, and (3)Footer. Flexbox was utilized to achieve this layout.
+    - The font-family 'Josefin Slab' was imported and applied on all texts.
+    - So far I have not learned intermediate CSS styling. But with the help of Google, I was able to apply basic hover effects.
 
-Will come back to this later to add UI.
+JavaScript
+    - I was able to utilize DOM manipulation to modify HTML elements and ID. Now I see why JavaScript is a must to have a dynamic website!
+    - Functions within functions, it was fun!
+
+Live demo: https://21ance.github.io/odin-rock-paper-scissors/
--- script.js
+++ script.js
@@ -1,46 +1,107 @@
 const choices = ["rock", "paper", 'scissors']
-let human = 0
-let computer = 0
-let draw = 0
+const buttons = document.querySelectorAll('button');
+const gameScore = document.querySelector('#gameScore')
+const reset = document.querySelector('#reset')
+const computerHand = document.querySelector('#computerHand')
+const computerIcon = document.querySelector('#computerIcon')
+const toggle = document.querySelector('#toggle')
+let computer
+let counterPlayer = 0
+let counterComputer = 0
+let counterDraw = 0
+let computerChoice
 
-function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)]
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        draw++
-        return "Draw"
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        human++
-        return `Player won, ${playerSelection} defeats ${computerSelection}`
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        human++
-        return `Player won, ${playerSelection} defeats ${computerSelection}`
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        human++
-        return `Player won, ${playerSelection} defeats ${computerSelection}`
+//accepts player and computer choices
+function playRound(player, computer) {
+    computer = computerPlay()
+    if (player == computer) {
+        counterDraw++
+    } else if (player == 'rock' && computer == 'scissors') {
+        counterPlayer++
+    } else if (player == 'paper' && computer == 'rock') {
+        counterPlayer++
+    } else if (player == 'scissors' && computer == 'paper') {
+        counterPlayer++
     } else {
-        computer++
-        return `Computer won, ${computerSelection} defeats ${playerSelection}`
+        counterComputer++
+    }
+    gameScore.textContent = `Player: ${counterPlayer} Computer: ${counterComputer} Draw: ${counterDraw}`
+    endGame(counterPlayer, counterComputer)
+}
+
+//computer choice randomizer
+function computerPlay() {
+    computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    computerSelect(computerChoice)
+    return computerChoice
+}
+
+//shows computer choice on a div,, useful for UI later
+function computerSelect(select) {
+    switch (select) {
+        case 'rock':
+            computerHand.textContent = 'Rock'
+            computerIcon.textContent = '✊'
+            break;
+        case 'paper':
+            computerHand.textContent = 'Paper'
+            computerIcon.textContent = '✋'
+            break;
+        case 'scissors':
+            computerHand.textContent = 'Scissors'
+            computerIcon.textContent = '✌️'
+    }
+    toggle.classList.add('computerToggle')
+}
+
+//reset values to default / restart game
+function resetGame() {
+    reset.addEventListener('click', () => {
+        counterPlayer = 0
+        counterComputer = 0
+        counterDraw = 0
+        gameScore.textContent = `Game Start!`
+        computerIcon.textContent = "?"
+        computerHand.textContent = "?"
+        toggle.classList.remove('computerToggle')
+        reset.textContent = "Reset"
+        buttons.forEach((button) => {
+            button.disabled = false
+        })
+    })
+}
+
+//
+function startGame() {
+    //access buttons with id rps only
+    buttons.forEach((button) => {
+        if (button.id == 'rock' || button.id == 'paper' || button.id == 'scissors') {
+            button.addEventListener('click', () => playRound(button.id, computerPlay));
+        }
+    });
+}
+
+//
+function endGame() {
+    if (counterPlayer == 5 || counterComputer == 5) {
+        buttons.forEach((button) => {
+            if (button.id == 'rock' || button.id == 'paper' || button.id == 'scissors') {
+                button.disabled = true
+                reset.textContent = 'Play Again?'
+                if(counterPlayer == 5){
+                    gameScore.textContent = `You Won! ${counterPlayer} : ${counterComputer}`
+                } else{
+                    gameScore.textContent = `You Lost... ${counterPlayer} : ${counterComputer}`
+                }
+            }
+        });
     }
 }
 
+//game...
 function game() {
-
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay()
-        let playerSelection = prompt(`Round ${i + 1}! Rock, Paper, Scissors?`).toLowerCase()
-
-        if(choices.includes(playerSelection)){
-            console.log(playRound(playerSelection, computerSelection));
-        } else{
-            alert("Please enter a valid input: 'Rock', 'Paper', or 'Scissors'")
-            i-- //feels like cheesing it :< in my defense I will remove this anyway after giving this code a UI
-        }
-
-    }
-    alert(`Player ${human}\nComputer ${computer}\nDraw ${draw}`)
+    startGame()
+    resetGame()
 }
 
 game()
